643398283c6f8aff181687c500627ef72089fa53
Paulocpjunior <p.c.pereira@me.com>
2026-03-09 09:20:57 -0300

    chore: downgrade vite and plugin-react for Node 18 compatibility and fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env
-  },
-  server: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.PORT || '3000'),
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.PORT || '3000'),
-  }
+    plugins: [react()],
+    define: {
+        'process.env': process.env
+    },
+    server: {
+        host: '0.0.0.0',
+        port: parseInt(process.env.PORT || '3000'),
+    },
+    preview: {
+        host: '0.0.0.0',
+        port: parseInt(process.env.PORT || '3000'),
+    }
 });
